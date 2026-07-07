2ee55db20124492ad0df8486208bd813bee4fc86
Robert <robertvatamanu21@gmail.com>
2026-07-07 12:11:00 +0300

    fix: adaugat output standalone pentru cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
+  // Adaugă această linie aici pentru OpenNext în producție:
+  output: 'standalone',
+  
   turbopack: {
     root: __dirname,
   },
